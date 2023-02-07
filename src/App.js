fe99cb8b16c2a66ee35d5a75414060334c45287a
Flavio <77132531+flaviodev27@users.noreply.github.com>
2023-02-07 20:39:22 -0300

    brinquei um pouquinho com animacao

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+
+
 
 function App() {
   return (
@@ -7,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          infinito e além
         </p>
         <a
           className="App-link"
