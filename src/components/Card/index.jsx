84b29b1cb1b726c34021e26797c25d39d3cadae6
Flavio <77132531+flaviodev27@users.noreply.github.com>
2023-02-10 17:29:16 -0300

    fiz a estrutura

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,6 @@
+import { Component } from "react";
+import './styles.css'
+
+export class Card extends Component {
+    
+}
--- src/components/TelaInicial/index.jsx
+++ src/components/TelaInicial/index.jsx
@@ -1,3 +1,6 @@
+import { Component } from "react"
+import './styles.css'
+
 export const TelaInicial = (props) => {
     console.log(props)
     return (
