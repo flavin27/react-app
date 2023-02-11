f9f4bd21c9abbd4d571ea6509e91be626d4606f6
Flavio <77132531+flaviodev27@users.noreply.github.com>
2023-02-11 19:54:43 -0300

    criei o header

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import Header from './components/header/Index'
 
 
 
 export default class App extends Component {
-  render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            infinito e além
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <>
+                <Header/>
+            </>
+        )
+    }
 }
-
-
