c8df49690ff5c4e0aac201b8916050c49c81d93a
Flavio <77132531+flaviodev27@users.noreply.github.com>
2023-02-22 20:22:01 -0300

    mudancas importantes!

--- src/components/header/Index.jsx
+++ src/components/header/Index.jsx
@@ -4,7 +4,7 @@ export default class Header extends Component{
     render() {
         return (
             <header>
-                <p>ola mundo!</p>
+                <p>Olá mundo!</p>
             </header>
         )
     }
