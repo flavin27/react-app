ad441a221f6affef97df69af0528391abc143fe1
Flavio <77132531+flaviodev27@users.noreply.github.com>
2023-02-23 22:46:42 -0300

    adicionei um <p>

--- src/components/header/Index.jsx
+++ src/components/header/Index.jsx
@@ -3,9 +3,15 @@ import React, { Component } from "react";
 export default class Header extends Component{
     render() {
         return (
-            <header>
-                <p>Olá mundo!</p>
-            </header>
+            <>
+                <header>
+                    <p>Olá mundo!</p>
+                </header>
+                <section>
+                    <p>To meio sem ideia.</p>
+                </section>
+            </>
+
         )
     }
 }
